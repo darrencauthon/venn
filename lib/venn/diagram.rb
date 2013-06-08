3cefd5b248b0fad74bf6fb1e8278172099b3fc6a
Darren Cauthon <darren@cauthon.com>
2013-06-08 15:03:10 -0500

    First idea of a syntax.

--- lib/venn/diagram.rb
+++ lib/venn/diagram.rb
@@ -1,5 +1,19 @@
 module Venn
   class Diagram
+    def a_is values; end
+    def b_is values; end
+
+    def a_only
+      [1, 2]
+    end
+
+    def b_only
+      [3, 4]
+    end
+
+    def a_and_b
+      []
+    end
   end
 end
 
--- spec/venn/diagram_spec.rb
+++ spec/venn/diagram_spec.rb
@@ -1,8 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Venn::Diagram do
+
+  let(:diagram) { Venn::Diagram.new }
+
   it "should exist" do
     Venn::Diagram.nil?.must_equal false
+  end
+
+  describe "simple situation" do
+
+    before do
+      diagram.a_is [1, 2]
+      diagram.b_is [3, 4]
+    end
+
+    it "should return set a" do
+      diagram.a_only.must_equal [1, 2]
+    end
+
+    it "should return set b" do
+      diagram.b_only.must_equal [3, 4]
+    end
+  
+    it "should return nothing for a and b" do
+      diagram.a_and_b.must_equal []
+    end
   end
 end
 
