c46335c87fefcb60b22b1affc22f93f9b618c5d9
Darren Cauthon <darren@cauthon.com>
2013-06-12 20:05:43 -0500

    No need to use symbols.

--- lib/venn/diagram.rb
+++ lib/venn/diagram.rb
@@ -7,15 +7,15 @@ module Venn
 
     def method_missing(meth, *args, &blk)
       if meth[-3, 3] == '_is'
-        id = meth[0, meth.length - 3].to_sym
+        id = meth[0, meth.length - 3]
         value = args[0]
         set_single_value id, value
       elsif meth[-4, 4] == '_are'
-        id = meth[0, meth.length - 4].to_sym
+        id = meth[0, meth.length - 4]
         value = args[0]
         set_single_value id, value
       elsif meth[-5, 5] == '_only'
-        id = meth[0, meth.length - 5].to_sym
+        id = meth[0, meth.length - 5]
         find_exclusive_values_in id
       else
         ins = meth.to_s.split('_and_')
@@ -37,7 +37,6 @@ module Venn
     end
 
     def find_the_and_matches(ins)
-      ins = ins.map { |x| x.to_sym }
       outs = @values.keys.select { |x| ins.include?(x) == false }
       matches = @values[ins.first].select do |x|
                   ins.select do |i|
